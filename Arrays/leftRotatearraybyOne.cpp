df74396a5daac59706e4891ad563af232da7fd10
shaurya1503 <chandrashaurya32@gmail.com>
2026-03-28 23:46:32 +0530

    Added solution for the question- left rotate(shift) an array by one position

---
+++ Arrays/leftRotatearraybyOne.cpp
@@ -0,0 +1,22 @@
+//Left rotate an array by one place
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cout<<"Enter Element "<<i<<" = ";
+        cin>>arr[i];
+    }
+    int temp=arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
