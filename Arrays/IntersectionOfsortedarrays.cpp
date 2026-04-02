3524aa26bf1111fdc68ebc2eaee25d8a25d16540
shaurya1503 <chandrashaurya32@gmail.com>
2026-04-02 20:24:45 +0530

    Added optimal solution for intersection of two sorted arrays

--- Arrays/IntersectionOfsortedarrays.cpp
+++ Arrays/IntersectionOfsortedarrays.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //Brute force approach where we make a third arrays to search if the element is repeated or not
-vector<int> Intersection(int arr1[],int arr2[], int n,int m){
+/*vector<int> Intersection(int arr1[],int arr2[], int n,int m){
     vector<int> result;
     int vis[m]={0};
     for(int i=0;i<n;i++){
@@ -12,6 +12,24 @@ vector<int> Intersection(int arr1[],int arr2[], int n,int m){
                 break;
             }
             if(arr1[j]>arr2[i]) break;
+        }
+    }
+    return result;
+}*/
+//Added optimal solution for intersection of sorted arrays by using to pointer method.
+vector<int> Intersection(int* arr1, int* arr2, int n,int m){
+    vector<int> result;
+    int i=0;
+    int j=0;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            i++;
+        }else if(arr1[i]>arr2[j]){
+            j++;
+        }else{
+            result.push_back(arr1[i]); //only to add element to intersection when they are equal
+            i++;
+            j++;
         }
     }
     return result;
