76a276f55ef280f655ca34a9557798f2a8cc2ef0
Shaurya Chandra <chandrashaurya32@gmail.com>
2026-07-07 18:13:43 +0530

    added time complexity and space complexity to better solution

--- Arrays/Majority Element-I/Better_solution.cpp
+++ Arrays/Majority Element-I/Better_solution.cpp
@@ -20,3 +20,6 @@ int main(){
     cout<<majority(nums)<<endl;
 return 0;
 }
+//Time Complexity: O(N), where N is the size of the input array. This is because we are iterating through the array once to count occurrences and then iterating through the hashmap to find the majority element.
+
+//Space Complexity: O(N), as we are using a hashmap to store the counts of each element, which can take up to N space in the worst case.
