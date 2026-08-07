4c491d9f7668ad1199993a5ed433baa94f6b8c4d
Shaurya Chandra <chandrashaurya32@gmail.com>
2026-08-07 11:47:50 +0530

    Refactor peak element function to return indices instead of values

--- Binary Search/Binary search on 1D arrays/Peak element/Optimal_solution.cpp
+++ Binary Search/Binary search on 1D arrays/Peak element/Optimal_solution.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int peakelement(vector<int>arr){
     int n=arr.size();
     if(n==1)return arr[0];
-    if(arr[0]>arr[1]) return arr[0];
-    if(arr[n-1]>arr[n-2]) return arr[n-1];
+    if(arr[0]>arr[1]) return 0;
+    if(arr[n-1]>arr[n-2]) return n-1;
     int low=1,high=n-2;
     while(low<=high) {
         int mid=low+(high-low)/2;
-        if(arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]) return arr[mid];
+        if(arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]) return mid;
         else if(arr[mid]<arr[mid+1])low=mid+1;
         else high =mid-1;
     }
@@ -18,7 +18,7 @@ int peakelement(vector<int>arr){
 }
 int main(){
     vector<int> arr={1,2,1,4,5,6,7,8,5,1};
-    cout<<"The peak element is "<<peakelement(arr);
+    cout<<"The peak element is at index  "<<peakelement(arr);
 }
 //Time complexity: O(log n) we reduce the search space in half at every step using binary search.
 //Space complexity: O(1) we are only using constants.
