096da6fffe07b30711634827a2b55c588eba5bbe
Shaurya Chandra <chandrashaurya32@gmail.com>
2026-08-07 11:39:46 +0530

    Added optimal solution for finding peak element in an array by using binary search algorithm

---
+++ Binary Search/Binary search on 1D arrays/Peak element/Optimal_solution.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int peakelement(vector<int>arr){
+    int n=arr.size();
+    if(n==1)return arr[0];
+    if(arr[0]>arr[1]) return arr[0];
+    if(arr[n-1]>arr[n-2]) return arr[n-1];
+    int low=1,high=n-2;
+    while(low<=high) {
+        int mid=low+(high-low)/2;
+        if(arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]) return arr[mid];
+        else if(arr[mid]<arr[mid+1])low=mid+1;
+        else high =mid-1;
+    }
+    return -1;
+
+}
+int main(){
+    vector<int> arr={1,2,1,4,5,6,7,8,5,1};
+    cout<<"The peak element is "<<peakelement(arr);
+}
+//Time complexity: O(log n) we reduce the search space in half at every step using binary search.
+//Space complexity: O(1) we are only using constants.
